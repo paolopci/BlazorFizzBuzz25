b282db27b71031ff079392de8f6ee298b40a6679
Paolo Paci <paolopci@yahoo.it>
2025-11-03 15:59:20 +0100

    insert model and fizzbuzz blazor page

---
+++ Models/FizzBuzz.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorFizzBuzz.Models
+{
+    public class FizzBuzz
+    {
+        public int FizzValue { get; set; }
+        public int BuzzValue { get; set; }
+        public int StopValue { get; set; }
+    }
+}
